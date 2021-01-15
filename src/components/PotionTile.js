881d82344e8fa051cdf15034f9b458217bb6796b
gerardmi24 <gerardmistretta@yahoo.com>
2021-01-15 09:49:18 -0500

    Working properly but inventory images are displaying strange

--- src/components/PotionTile.js
+++ src/components/PotionTile.js
@@ -8,8 +8,16 @@ Consider what the tiles have in common and what they do not:
 - For things that are different, consider how you might make use of props and JSX to have the same component behave differently when rendered by different parents 
 */
 
-function PotionTile(props) {
-  return <div>PotionTile</div>;
+function PotionTile({potion, children, buyOrSell, column}) {
+  console.log("Add Potion Image", potion.image_url)
+  return <div>
+    <div className={column === "Inventory" ? "inventory-card" : "card"}>
+      <div onClick={() => buyOrSell(potion.id)} className="image-wrapper">
+        <img className="image" alt={potion.name} src={potion.image_url} />
+      </div>
+      {children}
+    </div>
+  </div>;
 }
 
 export default PotionTile;
